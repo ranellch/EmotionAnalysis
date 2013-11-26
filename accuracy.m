ae643867a551ce0b35c4f8eb511690856ac4b1b8
Chris Ranella <ranellch@umich.edu>
2013-11-25 19:51:33 -0500

    accuracy

---
+++ accuracy.m
@@ -0,0 +1,28 @@
+%a test excel list
+%third column in excel is what program identifies picture as
+
+%output of classification code
+%look for file names and see what string was
+%compare to string we outputted
+
+function accuracy = get_accuracy(emotion, picture)
+    [NUM,file]=xlsread('testXL.xlsx');
+    total_correct = 0;
+    total = 0;
+    blank = '';
+    for i = 1:4
+        emotionInFile = file{i,2};
+        emotionDeclared = file{i,3};
+        if (strcmp(emotion, emotionInFile))   
+            if (~(strcmp(emotionDeclared,blank)))
+                if (strcmp(emotionInFile,emotionDeclared))
+                     total_correct = total_correct+1;
+                end
+                total = total+1;
+            end
+        end
+   end
+%number we got/total number in excel *100
+    accuracy = total_correct/total *100;
+
+end
